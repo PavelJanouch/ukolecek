60fdebd386bb447e9ce66be4bb5e4884e2756652
PavelJanouch <134501621+PavelJanouch@users.noreply.github.com>
2025-05-14 10:42:28 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'vehicles',
+    'username'  => 'root',
+    'password'  => '', // změň podle potřeby
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
